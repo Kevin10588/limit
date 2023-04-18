d878bf441ca3e070d340bd52bbb68e8353506066
lxAnxietyxl <villatorokevin42@gmail.com>
2023-04-18 15:30:01 -0700

    Revert "Revert "David Pushes 4/15/23""
    This reverts commit 3395ccd42f541b541583970a752b04ced91b16c0.

---
+++ Firebase/Firebase/Feature/Games/MathGameManager/MathGameManager.swift
@@ -0,0 +1,13 @@
+//
+//  MathGameManager.swift
+//  FirebaseTest
+//
+//  Created by David De La Cruz on 4/14/23.
+//
+
+import Foundation
+import SwiftUI
+
+class MathGameManager: ObservableObject {
+    
+}
