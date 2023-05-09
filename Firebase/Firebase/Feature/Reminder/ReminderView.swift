19a72ce886888736c9b84a788140533d5f8473ff
Luis Montalvo <lmontalvo1998@gmail.com>
2023-05-09 15:21:45 -0700

    Changed Badge Colors

--- Firebase/Firebase/Feature/Reminder/ReminderView.swift
+++ Firebase/Firebase/Feature/Reminder/ReminderView.swift
@@ -50,16 +50,19 @@ struct ReminderView: View {
             }
         }
     func badgeView() -> some View {
+        let bronzeColor = Color(hue: 0.08, saturation: 0.66, brightness: 0.66)
+        let silverColor = Color(hue: 0.55, saturation: 0.01, brightness: 0.80)
+        let goldColor = Color(hue: 0.14, saturation: 0.84, brightness: 0.90)
             if score.currentScore > 100 {
-                return BadgeView(color: .blue, text: "Very Productive",imageName: "Gold" , score: score)
+                return BadgeView(color: goldColor, text: "Very Productive",imageName: "Gold" , score: score)
             } else if score.currentScore > 30 {
-                return BadgeView(color: .blue, text: "Productive", imageName: "Silver", score: score)
+                return BadgeView(color: silverColor, text: "Productive", imageName: "Silver", score: score)
             } else if score.currentScore > 15 {
-                return BadgeView(color: .blue, text: "Doing Good", imageName: "Silver" , score: score)
+                return BadgeView(color: silverColor, text: "Doing Good", imageName: "Silver" , score: score)
             } else if score.currentScore < 0 {
                 return BadgeView(color: .red, text: "Unproductive", imageName: "logo" , score: score)
             } else {
-                return BadgeView(color: .blue, text: "Focus!", imageName: "Bronze" , score: score)
+                return BadgeView(color: bronzeColor, text: "Focus!", imageName: "Bronze" , score: score)
             }
         }
 }
